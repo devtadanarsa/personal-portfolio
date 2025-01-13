8e2b20ce8bad3ed8cbd3bf27e47230bcb954a5b2
devtadanarsa <devtadanarsa69@gmail.com>
2025-01-13 17:05:37 +0800

    feat: typewriter and cursor effect on header

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import ErrorPage from "./pages/ErrorPage";
 import AuthProvider from "./contexts/AuthContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 import ScrollToTop from "./utils/ScrollToTop";
+import AnimatedCursor from "react-animated-cursor";
 
 const App = () => {
   useEffect(() => {
@@ -21,38 +22,52 @@ const App = () => {
   }, []);
 
   return (
-    <ThemeProvider>
-      <AuthProvider>
-        <BrowserRouter>
-          <ScrollToTop />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route
-              path="/teams"
-              element={
-                <ProtectedRoute>
-                  <TeamPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/movies"
-              element={
-                <ProtectedRoute>
-                  <MoviePage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/access-error"
-              element={<ErrorPage errorCode={401} />}
-            />
-            <Route path="*" element={<ErrorPage errorCode={404} />} />
-          </Routes>
-        </BrowserRouter>
-      </AuthProvider>
-    </ThemeProvider>
+    <>
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={35}
+        innerScale={1}
+        outerScale={2}
+        outerAlpha={0}
+        // hasBlendMode={true}
+        innerStyle={{
+          backgroundColor: "#7456FF",
+        }}
+        outerStyle={{
+          mixBlendMode: "exclusion",
+          border: "3px solid #7456FF",
+        }}
+      />
+      <ThemeProvider>
+        <AuthProvider>
+          <BrowserRouter>
+            <ScrollToTop />
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/about" element={<AboutPage />} />
+              <Route
+                path="/teams"
+                element={
+                  <ProtectedRoute>
+                    <TeamPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/movies"
+                element={
+                  <ProtectedRoute>
+                    <MoviePage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/access-error" element={<ErrorPage errorCode={401} />} />
+              <Route path="*" element={<ErrorPage errorCode={404} />} />
+            </Routes>
+          </BrowserRouter>
+        </AuthProvider>
+      </ThemeProvider>
+    </>
   );
 };
 
